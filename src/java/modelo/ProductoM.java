58f72190474b716d298aa52c415b0f9a01145f34
Juan Condori Jara <72884565+juancondorijara@users.noreply.github.com>
2021-05-03 13:13:53 -0500

    Web, src y librerías

---
+++ src/java/modelo/ProductoM.java
@@ -0,0 +1,71 @@
+package modelo;
+
+import java.sql.Date;
+
+public class ProductoM {
+    
+    private int codigo;
+    private String nombre;
+    private String marca;
+    private String familia;
+    private String descripcion;
+    private String precio;
+    private Date vencimiento;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(String familia) {
+        this.familia = familia;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public Date getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(Date vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+
+}
